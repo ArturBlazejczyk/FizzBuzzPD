cebed7d29f5017657abcf6539e2f76189afd7507
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-08 18:35:05 +0100

    Add project files.

---
+++ FizzBuzzPD/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FizzBuzzPD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Wyświetlenie powitania
+            // 2. Przekazanie liczby przez użytkownika
+            // 3. Jeżeli liczba użytkownika będzie podzielna przez 3 bez reszty, to powinien zostać zwrócony wynik „Fizz”
+            // 4. Jeżeli liczba użytkownika będzie podzielna przez 5 bez reszty, to powinien zostać zwrócony wynik „Buzz”
+            // 5. Jeżeli liczba użytkownika będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to powinien zostać zwrócony wynik „FizzBuzz”
+            // Niech to będzie nowa metoda niestatyczna w osobnej klasie o nazwie FizzBuzz. Najlepiej jak sama metoda będzie zwracała string’a, który zostanie zwrócony i wyświetlony w klasie Program w metodzie Main.
+
+        }
+    }
+}

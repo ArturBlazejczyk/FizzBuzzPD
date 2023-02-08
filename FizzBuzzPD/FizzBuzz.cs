de37b3454f20f3cb3ceaa86fa30809efc4caad33
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-08 18:47:18 +0100

    basic logic in main program

---
+++ FizzBuzzPD/FizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPD
+{
+    class FizzBuzz
+    {
+        public string GetFizzBuzzResult(int input)
+        {
+            // 3. Jeżeli liczba użytkownika będzie podzielna przez 3 bez reszty, to powinien zostać zwrócony wynik „Fizz”
+            // 4. Jeżeli liczba użytkownika będzie podzielna przez 5 bez reszty, to powinien zostać zwrócony wynik „Buzz”
+            // 5. Jeżeli liczba użytkownika będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to powinien zostać zwrócony wynik „FizzBuzz”
+
+            return "Udało się";
+        }
+    }
+}
--- FizzBuzzPD/Program.cs
+++ FizzBuzzPD/Program.cs
@@ -6,16 +6,15 @@ namespace FizzBuzzPD
     {
         static void Main(string[] args)
         {
-            // 1. Wyświetlenie powitania
             Console.WriteLine("Witaj w programie \"Fizz Buzz\"!");
-            // 2. Przekazanie liczby przez użytkownika
             Console.WriteLine("Proszę o podanie liczby: ");
+
             if (!int.TryParse(Console.ReadLine(), out int input))
                 throw new ArgumentOutOfRangeException();
-            // 3. Jeżeli liczba użytkownika będzie podzielna przez 3 bez reszty, to powinien zostać zwrócony wynik „Fizz”
-            // 4. Jeżeli liczba użytkownika będzie podzielna przez 5 bez reszty, to powinien zostać zwrócony wynik „Buzz”
-            // 5. Jeżeli liczba użytkownika będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to powinien zostać zwrócony wynik „FizzBuzz”
-            // Niech to będzie nowa metoda niestatyczna w osobnej klasie o nazwie FizzBuzz. Najlepiej jak sama metoda będzie zwracała string’a, który zostanie zwrócony i wyświetlony w klasie Program w metodzie Main.
+
+            var fizzBuzz = new FizzBuzz();
+
+            Console.WriteLine(fizzBuzz.GetFizzBuzzResult(input));
 
         }
     }

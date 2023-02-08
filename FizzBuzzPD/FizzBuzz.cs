f84bff73b5e4eb977dd58b8566bc6f206d3b59f7
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-08 18:58:38 +0100

    logic for method added

--- FizzBuzzPD/FizzBuzz.cs
+++ FizzBuzzPD/FizzBuzz.cs
@@ -11,7 +11,15 @@ namespace FizzBuzzPD
         public string GetFizzBuzzResult(int input)
         {
             // 3. Jeżeli liczba użytkownika będzie podzielna przez 3 bez reszty, to powinien zostać zwrócony wynik „Fizz”
+            if(input%3 == 0) 
+            {
+                return "Fizz";
+            }
             // 4. Jeżeli liczba użytkownika będzie podzielna przez 5 bez reszty, to powinien zostać zwrócony wynik „Buzz”
+            if(input%5 == 0) 
+            {
+                return ""
+            }
             // 5. Jeżeli liczba użytkownika będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to powinien zostać zwrócony wynik „FizzBuzz”
 
             return "Udało się";

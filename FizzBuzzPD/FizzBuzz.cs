b35e11ade69837e152a587bdb70355c4bea8b6a4
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-08 18:58:53 +0100

    changes added

--- FizzBuzzPD/FizzBuzz.cs
+++ FizzBuzzPD/FizzBuzz.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FizzBuzzPD
 {
@@ -10,19 +11,14 @@ namespace FizzBuzzPD
     {
         public string GetFizzBuzzResult(int input)
         {
-            // 3. Jeżeli liczba użytkownika będzie podzielna przez 3 bez reszty, to powinien zostać zwrócony wynik „Fizz”
-            if(input%3 == 0) 
-            {
+            if (input % 3 == 0 && input % 5 == 0)
+                return "FizzBuzz";
+            if (input % 3 == 0)
                 return "Fizz";
-            }
-            // 4. Jeżeli liczba użytkownika będzie podzielna przez 5 bez reszty, to powinien zostać zwrócony wynik „Buzz”
-            if(input%5 == 0) 
-            {
-                return ""
-            }
-            // 5. Jeżeli liczba użytkownika będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to powinien zostać zwrócony wynik „FizzBuzz”
-
-            return "Udało się";
+            if (input % 5 == 0)
+                return "Buzz";
+            else
+                return input.ToString();
         }
     }
 }
